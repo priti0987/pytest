b0f3e5f8b5fb2341b2f0aa2b392f96b21b084dbf
priti <pritibhushanf@gmail.com>
2025-08-16 21:02:21 +0530

    16Aug

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -1,7 +1,27 @@
 import pytest
 from selenium import webdriver
 
+
+
+def pytest_addoption(parser):
+    parser.addoption("--my-browser",action="store",default="chrome",
+                     help="Specify the browser or firefox or edge ")
+
+
 @pytest.fixture()
-def setup():
-    driver = webdriver.Chrome()
+def browser(request):
+    return request.config.getoption("--my-browser")
+
+
+@pytest.fixture()
+def setup(browser):
+    global driver
+    if browser=="chrome":
+        driver = webdriver.Chrome()
+    elif browser == 'firefox':
+        driver = webdriver.Firefox()
+    elif browser == 'edge':
+        driver = webdriver.Edge()
+    else:
+        raise ValueError("Unsupported browser")
     return driver
